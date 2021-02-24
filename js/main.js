195674c3a08f942e94b61d7c360c21edbe098818
Mateusz Malec <m.mateusz2000@wp.pl>
2021-02-24 14:18:28 +0100

    add info alert

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 'user strict';
 const field = document.querySelector('.field');
 const cards = document.querySelectorAll('.card-back');
+const allCards = document.querySelectorAll('.card');
 const tries = document.querySelector('.tries');
 const pairs = document.querySelector('.pairs');
-
+const boxAlert = document.querySelector('.box-alert');
+const alert = document.querySelector('.alert-text');
+const alertBtn = document.querySelector('.alert-button');
 
 let $allPairs = 6;
-let $cardsNumber = 12;
 let $clicks = 1;
 let $firstEl;
 let $secondEl;
@@ -16,6 +18,7 @@ let $pairs = 1;
 let $tries = 1;
 
 const assignPictures = () => {
+    let cardsNumber = 12;
     let nums = [];
     let ranNums = [];
 
@@ -30,15 +33,15 @@ const assignPictures = () => {
     }
 
     cards.forEach(el => {
-        $cardsNumber--;
-        el.classList.add(ranNums[$cardsNumber]);
-        el.style.backgroundImage = 'url(/img/' + ranNums[$cardsNumber] + '.jpg)';
+        cardsNumber--;
+        el.classList.add(ranNums[cardsNumber]);
+        el.style.backgroundImage = 'url(/img/' + ranNums[cardsNumber] + '.jpg)';
     });
 }
 assignPictures();
 
 
-const active = (e) => {
+const active = e => {
 
     const el = e.target.closest('.card');
 
@@ -60,20 +63,40 @@ const active = (e) => {
 }
 
 const compare = () => {
-
     if ($toCompare === $toCompare2) {
-        $firstEl.style.transform = 'scale(.5)';
-        $secondEl.style.transform = 'scale(.5)';
+        $firstEl.style.transform = 'scale(.6)';
+        $secondEl.style.transform = 'scale(.6)';
         pairs.innerText = $pairs;
         tries.innerText = $tries;
+
+        if ($pairs === $allPairs) {
+            showAlert();
+        }
+
         $tries++;
         $pairs++;
     } else {
         $firstEl.classList.remove('rotate');
         $secondEl.classList.remove('rotate');
         tries.innerText = $tries;
-        $tries++;
+
+        $tries >= 30 ? showAlert() : $tries++;
+    }
+}
+
+const showAlert = () => {
+    if ($pairs === $allPairs) {
+        boxAlert.style.visibility = 'visible';
+        alert.style.color = 'yellowgreen';
+        alert.innerText = 'wygrałeś!';
+
+    } else {
+        boxAlert.style.visibility = 'visible';
+        alert.style.color = 'tomato';
+        alert.innerText = 'przegrałeś!';
+
     }
 }
 
 field.addEventListener('click', active);
+
